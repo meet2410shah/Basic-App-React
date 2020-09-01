a6bbd047f08c7336ba7ed3f831c17f43fe314629
Meet Shah <44209311+meet2410shah@users.noreply.github.com>
2020-09-01 15:16:00 +0530

    setup-done

---
+++ README.md
@@ -0,0 +1,8 @@
+###
+
+- `npm i -D webpack webpack-cli`
+- `npm i -D html-webpack-plugin html-loader`
+- `npm i -D webpack-dev-server`
+- `npm i -D @babel/core babel-loader @babel/preset-env`
+- `npm i -D @babel/core babel-loader @babel/preset-env @babel/preset-react babel-plugin-transform-class-properties babel-preset-es2015`
+- `npm i -D file-loader`
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,46 @@
-const path = require('path');
-
-const DIST_DIR = path.resolve(__dirname, 'dist');
-const SRC_DIR = path.resolve(__dirname, 'src');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-	entry: SRC_DIR + '/app/index.js',
-	output: {
-		path: DIST_DIR + '/app',
-		filename: 'bundle.js',
-		publicPath: '/app/',
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.html$/,
+				use: [
+					{
+						loader: 'html-loader',
+						options: { minimize: true },
+					},
+				],
+			},
+			{
+				test: /\.(png | svg | jpg | gif)$/,
+				use: ['file-loader'],
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, 'css-loader'],
+			},
+		],
 	},
-	optimization: {
-		minimize: true,
-	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin(),
+	],
+	performance: { hints: false },
 };
